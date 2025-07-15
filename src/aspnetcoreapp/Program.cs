1b24c1effa5920f8b70eb55bdcae2086a6e4f3d3
Florian Suen <floriansuen@gmail.com>
2025-07-15 16:54:23 +0800

    Database setups and html template tests

--- src/aspnetcoreapp/Program.cs
+++ src/aspnetcoreapp/Program.cs
@@ -1,22 +1,34 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using aspnetcoreapp.Data;
+ 
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+builder.Services.AddDbContext<AspnetcoreappContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("aspnetcoreappContext") ?? throw new InvalidOperationException("Connection string 'aspnetcoreappContext' not found.")));
 
+ 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
+    app.UseHsts();
 }
 
+app.UseHttpsRedirection();
 app.UseRouting();
 
 app.UseAuthorization();
 
 app.MapStaticAssets();
 app.MapRazorPages()
-   .WithStaticAssets();
+.WithStaticAssets();
 
 app.Run();
+
